3669429f1a2cde1d3624ea232faf8f734ef61127
rohit <rohit.880207@gmail.com>
2025-02-22 16:52:03 +0530

    made new table work with more columns from tasks

--- database.ts
+++ database.ts
@@ -5,18 +5,21 @@ export const createTable = async () => {
 
     await db.execAsync(`
         PRAGMA journal_mode = WAL;
-        CREATE TABLE IF NOT EXISTS tasks (title TEXT NOT NULL, time TEXT, percentage TEXT);
+        CREATE TABLE IF NOT EXISTS work (date TEXT,title TEXT NOT NULL, duration TEXT, percentage INTEGER, tag TEXT);
         `);
     return  db
 };
 
-export const addTask = async (title:string,time:string) => {
-    const result = await db.runAsync('INSERT INTO tasks (title, time) VALUES (?, ?)', title, time);
-    console.log(result.lastInsertRowId, result.changes);
+export const addTask = async (date:string,title:string,duration:string,percentage:number,tag:string) => {
+    const result = await db.runAsync(
+        'INSERT INTO work (date, title, duration, percentage, tag) VALUES (?, ?, ?, ?, ?)'
+        , date, title, duration, percentage, tag);
+    return result
+    
 }
 
 export const getTasks = async() => {
-    const allRows:Tasks[] = await db.getAllAsync('SELECT * FROM tasks');
+    const allRows:Tasks[] = await db.getAllAsync('SELECT * FROM work');
     return allRows
 }
 
