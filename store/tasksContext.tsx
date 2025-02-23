890acd530ab56675d2bf4ee49b9b869bdc68f77f
rohit <rohit.880207@gmail.com>
2025-02-23 18:59:10 +0530

    added contexts for hours

--- store/tasksContext.tsx
+++ store/tasksContext.tsx
@@ -1,36 +1,58 @@
 // context/TasksContext.tsx
 
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { getTasks, addTask } from '../database';
+import { getTasks, addTask, getTotalProductiveHours, getTotalUnproductiveHours, getTotalMissingHours, getTotalNeutralHours } from '../database';
 
 
 interface TasksContextType {
     tasks: Tasks[];
     fetchTasks: () => Promise<void>;
     createTask: (date:string,title: string, duration: string, percentage:number,tag:string) => Promise<void>;
+    productive:number;
+    unproductive:number;
+    neutral:number;
+    missing:number;
 }
 
 const TasksContext = createContext<TasksContextType | undefined>(undefined);
 
 export const TasksProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [tasks, setTasks] = useState<Tasks[]>([]);
+    const [productive, setProductive] = useState<number>(0);
+    const [unproductive, setUnproductive] = useState<number>(0);
+    const [neutral, setNeutral] = useState<number>(0);
+    const [missing, setMissing] = useState<number>(0);
 
     const fetchTasks = async () => {
         const allTasks = await getTasks();
+        fetchProductive();
         setTasks(allTasks);
     };
 
     const createTask = async (date:string,title: string, duration: string, percentage:number,tag:string) => {
         await addTask(date,title, duration, percentage, tag);
+        fetchProductive();
         await fetchTasks(); // Refresh the task list after adding a new task
     };
 
+    const fetchProductive = async () => {
+        const productive : any = await getTotalProductiveHours();
+        const Unproductive : any = await getTotalUnproductiveHours();
+        const neutral : any = await getTotalNeutralHours();
+        const missing : any = await getTotalMissingHours();
+        setProductive(productive[0].total);
+        setUnproductive(Unproductive[0].total);
+        setNeutral(neutral[0].total);
+        setMissing(missing);
+    }
+
     useEffect(() => {
         fetchTasks(); // Fetch tasks when the provider mounts
+        fetchProductive();
     }, []);
 
     return (
-        <TasksContext.Provider value={{ tasks, fetchTasks, createTask }}>
+        <TasksContext.Provider value={{ tasks, fetchTasks, createTask,productive,unproductive,neutral,missing }}>
             {children}
         </TasksContext.Provider>
     );
