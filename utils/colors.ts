d8f25726bb9242a175f5228e74b98a8a1046387d
rohit <rohit.880207@gmail.com>
2025-02-23 18:31:53 +0530

    added solid,positive,negative and neutral colors

--- utils/colors.ts
+++ utils/colors.ts
@@ -5,13 +5,22 @@ const theme = {
         text: 'white',
         background: '#23252D',
         primary: '#1EB3F3',
-        border: '#D3D3D3'
+        border: '#D3D3D3',
+        solid:'white',
+        positive:'#00C896',
+        negative:'#E05E5E',
+        neutral:'#808080',
+
     },
     light: {
         text: '#454545',
         background: 'white',
         primary: '#1EB3F3',
-        border: '#D3D3D3'    
+        border: '#D3D3D3',
+        solid:'black',
+        positive:'#00C896',
+        negative:'#E05E5E',
+        neutral:'#808080',
 
     }
   };
