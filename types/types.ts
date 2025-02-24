e2d203e33c12fb08bc15f8e5ddb6073d29420107
rohit <rohit.880207@gmail.com>
2025-02-24 13:49:03 +0530

    update tasks type and added id

--- types/types.ts
+++ types/types.ts
@@ -1,11 +1,12 @@
 type Tasks = {   
-    date?:string;
-    percentage?:number;
+    id:number;
+    date:string;
+    percentage:number;
     title: string;  
     duration: number;
-    tag?:string;
-    start_time?:string;
-    end_time?:string;
+    tag:string;
+    start_time:string;
+    end_time:string;
 }
 
 type TableProps = {
