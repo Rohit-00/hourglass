18e41f6ddbb3a42786c74cc57e1b6aabbd698051
rohit <rohit.880207@gmail.com>
2025-02-24 13:47:17 +0530

    added bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
-import { TimeRem } from "./components/timeRem";
-import Table from "./components/table";
+import { TimeRem } from "./src/components/timeRem";
+import Table from "./src/components/table";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import BottomSheet, { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from "@gorhom/bottom-sheet";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { ChangeTimeForm } from "./components/changeTimeForm";
-import { AddTask } from "./components/addTask";
+import { ChangeTimeForm } from "./src/components/changeTimeForm";
+import { AddTask } from "./src/components/addTask";
 import { addTask, createTable, getTasks } from "./database";
 import { TasksProvider } from "./store/tasksContext";
 import { TimeProvider } from "./store/timeContext";
-import BentoGrid from "./components/bentoGrid";
+import BentoGrid from "./src/components/bentoGrid";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./src/screens/home";
+import { NavigationContainer } from "@react-navigation/native";
+import History from "./src/screens/history";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
@@ -23,57 +29,22 @@ export default function App() {
     initDB()
   })
 
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const taskBottomSheetModalRef = useRef<BottomSheetModal>(null);
-
-  // Store the modal open function instead of calling it directly
-  const [handlePresentModalPress, setHandlePresentModalPress] = useState<(() => void) | null>(null);
-
-  // Function to receive callbacks from child
-  const handleReceiveChildFunctions = (callbacks: {
-    handlePresentModalPress: () => void;
-    handleSheetChanges: (index: number) => void;
-  }) => {
-    setHandlePresentModalPress(() => callbacks.handlePresentModalPress);
-  };
 
 
   return (
+
     <TimeProvider> 
     <TasksProvider>
-    <GestureHandlerRootView style={styles.safeArea}>
-      <SafeAreaView>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <StatusBar style="auto" />
-          <View style={styles.container}>
-            <TimeRem bottomSheetModalRef={bottomSheetModalRef} sendFunctionsToParent={handleReceiveChildFunctions} />
-            <BentoGrid/>
-            <Table heading="What Have I Done Today" bottomSheetModalRef={taskBottomSheetModalRef} sendFunctionsToParent={handleReceiveChildFunctions}/>
-            
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+    <GestureHandlerRootView>
+    <NavigationContainer>
+    <Tab.Navigator screenOptions={{headerShown:false}}>
 
-      {/* Bottom Sheet for changing bed and wake up time */}
-      <BottomSheetModalProvider>
-        <BottomSheetModal ref={bottomSheetModalRef} aria-hidden>
-          <BottomSheetView style={styles.contentContainer}>
-            <ChangeTimeForm bottomSheetModalRef={bottomSheetModalRef} />
-          </BottomSheetView>
-        </BottomSheetModal>
-      </BottomSheetModalProvider>
-
-
-      {/* Bottom Sheet for adding tasks */}
-      <BottomSheetModalProvider>
-        <BottomSheetModal ref={taskBottomSheetModalRef} aria-hidden>
-          <BottomSheetView style={styles.contentContainer}>
-           <AddTask bottomSheetModalRef={taskBottomSheetModalRef}/>
-          </BottomSheetView>
-        </BottomSheetModal>
-      </BottomSheetModalProvider>
-
-    </GestureHandlerRootView>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="History" component={History}/>
+    </Tab.Navigator>
+    {/* <Home/> */}
+    </NavigationContainer>
+     </GestureHandlerRootView>
     </TasksProvider>
     </TimeProvider>
   );
