58e6a3671ae232a180b02f5ad205de6de91e2114
rohit <rohit.880207@gmail.com>
2025-02-27 10:50:15 +0530

    added functions to calculate this month, last month and this week

--- utils/dateHelpers.ts
+++ utils/dateHelpers.ts
@@ -90,3 +90,24 @@ export function convertToTimeDuration(hours: number): string {
   
     return result.trim();
   }
+
+  export const calcWeek = () => {
+    const today = new Date();
+    today.setDate(today.getDate() - 7);
+    return today;
+  };
+  
+  // Calculate this month's first date
+  export const getFirstThisMonth = () => {
+    const today = new Date();
+    const firstDateOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    return firstDateOfMonth;
+  };
+  
+  // Calculate previous month's first date
+  export const getFirstLastMonth = () => {
+    const today = new Date();
+    const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+    return lastMonth;
+  };
+  
