0ab1554d87a99e26f4c0f9a686c669831067820b
rohit <rohit.880207@gmail.com>
2025-03-03 12:48:51 +0530

    styled bottom tab navigation

--- App.tsx
+++ App.tsx
@@ -5,16 +5,18 @@ import Table from "./src/components/table";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import BottomSheet, { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from "@gorhom/bottom-sheet";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { ChangeTimeForm } from "./src/components/changeTimeForm";
-import { AddTask } from "./src/components/addTask";
+import Entypo from '@expo/vector-icons/Entypo';
 import { addTask, createTable, getTasks } from "./database";
 import { TasksProvider } from "./store/tasksContext";
 import { TimeProvider } from "./store/timeContext";
 import BentoGrid from "./src/components/bentoGrid";
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./src/screens/home";
 import { NavigationContainer } from "@react-navigation/native";
 import History from "./src/screens/history";
+import { colors } from "./utils/colors";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 const Tab = createBottomTabNavigator();
 
@@ -32,13 +34,27 @@ export default function App() {
 
 
   return (
-
     <TimeProvider> 
     <TasksProvider>
     <GestureHandlerRootView>
     <NavigationContainer>
-    <Tab.Navigator screenOptions={{headerShown:false}}>
-
+    <Tab.Navigator screenOptions={({route})=>({
+      tabBarStyle:{
+        height:60, 
+        paddingTop:5,
+        borderWidth:0.5,
+        borderColor:colors.border
+      },
+  
+      tabBarActiveTintColor:colors.primary,
+      headerShown:false,
+      tabBarIcon:({color, size}) => {
+        if(route.name==="Home") 
+        return <FontAwesome5 name={"home"} size={size} color={color} />
+        else
+        return <AntDesign name="clockcircle" size={size} color={color} />
+      }
+    })}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="History" component={History}/>
     </Tab.Navigator>
