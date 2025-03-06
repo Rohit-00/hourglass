9aa3ba4eb372d45ccad2366e1e4274dfdad043a7
rohit <rohit.880207@gmail.com>
2025-03-06 13:16:01 +0530

    made metrices functional

--- src/components/prodData.tsx
+++ src/components/prodData.tsx
@@ -1,24 +1,58 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { colors } from '../../utils/colors'
+import { useTasks } from '../../store/tasksContext';
+import { calcWeek, getFirstThisMonth } from '../../utils/dateHelpers';
+import { getLastMonthResultsNumber } from '../../database';
 
 const ProdData = () => {
+    const {allResults} = useTasks();
+
+    const [last7days, setLast7days] = useState<number>()
+    const [thisMonth, setThisMonth] = useState<number>()
+    const [lastMonth, setLastMonth] = useState<number>()
+
+    useEffect(()=>{
+        const last7 = allResults.filter((item) => {
+            const [month, day, year] = item.date.split('/').map(Number);
+            const itemDate = new Date(year, month - 1, day); 
+           
+            return itemDate >= calcWeek() && itemDate <= new Date() && item.result === "Productive";
+        });
+
+        setLast7days(last7.length)
+
+        const ThisMonth = allResults.filter((item) => {
+            const [month, day, year] = item.date.split('/').map(Number);
+            const itemDate = new Date(year, month - 1, day); 
+           
+            return itemDate >= getFirstThisMonth() && itemDate <= new Date() && item.result === "Productive";
+        });
+        setThisMonth(ThisMonth.length)
+
+        const getLastMonth = async() => {
+            const data = await getLastMonthResultsNumber()
+            setLastMonth(data)
+        }
+        getLastMonth()
+    },[])
+  
   return (
     <View>
       <Text style={styles.heading}>Total Productive Days</Text>
       <View style={styles.metricsContainer}>
         <View style={styles.metricData}>
-            <Text style={styles.data}>12</Text>
+            <Text style={styles.data}>{thisMonth}</Text>
             <Text style={styles.dataInfo}>This Month</Text>
         </View>
         <View style={{height:'70%',width:1,backgroundColor:colors.text}}/>
         <View style={styles.metricData}>
-            <Text style={styles.data}>12</Text>
-            <Text style={styles.dataInfo}>This Week</Text>
+            <Text style={styles.data}>{last7days}</Text>
+            <Text style={styles.dataInfo}>Last 7 days</Text>
         </View>
         <View style={{height:'70%',width:1,backgroundColor:colors.text}}/>
         <View style={styles.metricData}>
-            <Text style={styles.data}>12</Text>
+            <Text style={styles.data}>{lastMonth}</Text>
             <Text style={styles.dataInfo}>Last Month</Text>
         </View>
       </View>
