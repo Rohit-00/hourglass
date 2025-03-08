db8a1e906b63acc437cbad15f766270cf71a80ca
rohit <rohit.880207@gmail.com>
2025-03-08 14:18:02 +0530

    added color for text,background,app

--- utils/colors.ts
+++ utils/colors.ts
@@ -2,25 +2,27 @@ import { Appearance } from "react-native";
 
 const theme = {
     dark: {
-        text: 'white',
-        background: '#23252D',
-        primary: '#1EB3F3',
+        text: '#EFEFEF',
+        background: '#252525',
+        primary: '#526BBB',
         border: '#D3D3D3',
         solid:'white',
         positive:'#00C896',
         negative:'#E05E5E',
         neutral:'#808080',
+        appBackground:'#1A1A1A'
 
     },
     light: {
-        text: '#454545',
+        text: '#272727',
         background: 'white',
-        primary: '#1EB3F3',
+        primary: '#526BBB',
         border: '#D3D3D3',
         solid:'black',
         positive:'#00C896',
         negative:'#E05E5E',
         neutral:'#808080',
+        appBackground:'#F8FAFB'
 
     }
   };
